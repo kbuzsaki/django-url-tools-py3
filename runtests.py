0b2fa9793c3089f2d70bb822ca1ec509425eea29
Branko Vukelic <branko@brankovukelic.com>
2013-03-05 00:06:10 +0100

    Modified runtest to run with nose

---
+++ runtests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+
+from django.conf import settings, global_settings
+
+from nose.plugins.plugintest import run_buffered as run
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        INSTALLED_APPS=[
+            'url_tools',
+        ],
+        ROOT_URLCONF='',
+        DEBUG=False,
+        SITE_ID=1,
+    )
+
+
+import sys
+from os.path import dirname, abspath
+from optparse import OptionParser
+from django.test.simple import DjangoTestSuiteRunner
+
+def runtests(*test_args, **kwargs):
+    if 'south' in settings.INSTALLED_APPS:
+        from south.management.commands import patch_for_test_db_setup
+        patch_for_test_db_setup()
+
+    if not test_args:
+        test_args = ['tests']
+
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+
+    run(argv=sys.argv)
+
+if __name__ == '__main__':
+    runtests()
+
