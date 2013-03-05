7856567e13c8eb04ebc959d9ef23f8a02ef44846
Branko Vukelic <branko@brankovukelic.com>
2013-03-05 03:13:06 +0100

    Added setup module

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='django-url-tools',
+    description='Django helpers for dealing with URLs in templates',
+    long_description=open('README.rst').read(),
+    version='0.0.1',
+    packages=['url_tools'],
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-url-tools',
+    download_url='https://bitbucket.org/monwara/django-url-tools/downloads',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
