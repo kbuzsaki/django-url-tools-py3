9fe270a752decceb5cbb13d9027396d54d79d91a
Branko Vukelic <branko@brankovukelic.com>
2013-03-05 04:45:43 +0100

    Facepalm: We actually need a context processor!
    Yeah, I admit to major stupidity. I implemented a middleware class instead of a
    context processor. The make the facepalm harder, the module _is_ called
    context_processors.

--- url_tools/context_processors.py
+++ url_tools/context_processors.py
@@ -3,8 +3,6 @@ from __future__ import absolute_import, unicode_literals
 from .helper import UrlHelper
 
 
-class CurrentUrlProcessor(object):
-    def process_template_response(self, request, response):
-        full_path = request.get_full_path()
-        response.context_data['current_url'] = UrlHelper(full_path)
-        return response
+def current_url(request):
+    full_path = request.get_full_path()
+    return dict(current_url=UrlHelper(full_path))
