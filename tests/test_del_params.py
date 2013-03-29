6461916e9a57ab8264f9c3e8b40312268619fd98
Branko Vukelic <branko@brankovukelic.com>
2013-03-29 19:48:00 +0100

    Added del_params template tag

---
+++ tests/test_del_params.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, unicode_literals
+
+from unittest import TestCase
+
+from url_tools.templatetags.urls import del_params
+from url_tools.helper import UrlHelper
+
+
+class DelParamsTestCase(TestCase):
+    def test_del_params_basic(self):
+        self.assertEqual(
+            del_params('/foo?bar=1&baz=2', 'baz'),
+            '/foo?bar=1'
+        )
+
+    def test_del_paramss_all(self):
+        self.assertEqual(
+            del_params('/foo?bar=1&baz=2'),
+            '/foo'
+        )
+
+    def test_del_params_takes_helper_instance(self):
+        self.assertEqual(
+            del_params(UrlHelper('/foo?bar=1&baz=2'), 'bar'),
+            '/foo?baz=2'
+        )
--- url_tools/templatetags/urls.py
+++ url_tools/templatetags/urls.py
@@ -18,6 +18,17 @@ def add_params(url, **kwargs):
         return ''
 
 
+@register.simple_tag
+def del_params(url, *args):
+    if type(url) in [str, unicode]:
+        url = UrlHelper(url)
+    try:
+        url.del_params(*args)
+        return url.get_full_path()
+    except:
+        return ''
+
+
 @register.assignment_tag
 def url_params(url, **kwargs):
     u = UrlHelper(url)
