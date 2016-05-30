28dbebca348bd65d4fa735516ee86f152a0708fc
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-05-30 14:55:29 -0700

    Updated setup.py with information relevant to fork

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='django-url-tools',
+    name='django-url-tools-py3',
     description='Django helpers for dealing with URLs in templates',
-    long_description=open('README.rst').read(),
-    version='0.0.8',
+    version='0.1.0.1',
     packages=['url_tools', 'url_tools.templatetags'],
     author='Monwara LLC',
     author_email='branko@monwara.com',
-    url='https://bitbucket.org/monwara/django-url-tools',
-    download_url='https://bitbucket.org/monwara/django-url-tools/downloads',
+    maintainer='Kyle Buzsaki',
+    maintainer_email='kbuzsaki@gmail.com',
+    url='https://github.com/kbuzsaki/django-url-tools-py3',
+    download_url='https://github.com/kbuzsaki/django-url-tools-py3/releases',
     license='BSD',
     classifiers = [
         'Development Status :: 2 - Pre-Alpha',
